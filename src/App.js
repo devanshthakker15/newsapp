b82312016239028e47a496d35e049976d40a8cda
devanshthakker15 <devanshthakker15@gmail.com>
2024-05-07 22:52:30 +0530

    Listing all news from sample output

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+
+export default class App extends Component {
+  c = 'John';
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <News/>
+        {/* Hello my first class based component {this.c} */}
+
+        
+      </div>
+    )
+  }
+}
