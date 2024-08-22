aaf12caefb076bd734b060ff813681635318050c
devanshthakker15 <devanshthakker15@gmail.com>
2024-08-22 12:19:38 +0530

    All cards on page

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     let {title,description, imageUrl, newsUrl} = this.props;
     return (
       <div className='my-3'>
-         <div className="card" style={{width: "18rem;"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+         <div className="card" style={{width: "18rem"}}>
+            <img src={!imageUrl?"https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2024/05/apple-let-loose-event-1715068806.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
-                <p className="card-text">{description}...</p>
+                <p className="card-text">{description}</p>
                 <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
